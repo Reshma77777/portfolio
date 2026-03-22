fca8e490d7c6173ad72d143b8c92eef807dee3f2
Reshma77777 <rjoy341@gmail.com>
2026-03-22 18:31:15 +0530

    Update settings.ts
    added

--- src/settings.ts
+++ src/settings.ts
@@ -3,7 +3,7 @@ export const profile = {
 	title: '',
 	institute: '',
 	author_name: '', // Author name to be highlighted in the papers section
-	research_areas: { title: 'Basal Cognition', description: 'Brief description of the research interest', field: 'Philosophy' },
+	research_areas: [ { title: 'Basal Cognition', description: 'Brief description of the research interest', field: 'Philosophy' },
 		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
 	],
 }
